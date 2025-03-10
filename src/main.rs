28fc7f05cb1a76a68f2a2bcfeb45505e8f8d3184
Erlend Tobiassen <erlend.tobiassen@gmail.com>
2025-03-10 16:29:32 +0100

    add licenses

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 // Copyright (c) 2016 The Rouille developers
+// Copyright (c) 2025 Erlend Tobiassen
 // Licensed under the Apache License, Version 2.0
 // <LICENSE-APACHE or
 // http://www.apache.org/licenses/LICENSE-2.0> or the MIT
